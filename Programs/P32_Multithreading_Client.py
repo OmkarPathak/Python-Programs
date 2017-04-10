0827bad86f251510a3295827bbd7c7c8889a7611
Omkar Pathak <omkarpathak27@gmail.com>
2017-04-10 08:39:26 +0530

    Python Programs

---
+++ Programs/P32_Multithreading_Client.py
@@ -0,0 +1,20 @@
+import socket
+
+ClientSocket = socket.socket()
+host = '127.0.0.1'
+port = 1233
+
+print('Waiting for connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+Response = ClientSocket.recv(1024)
+while True:
+    Input = input('Say Something: ')
+    ClientSocket.send(str.encode(Input))
+    Response = ClientSocket.recv(1024)
+    print(Response.decode('utf-8'))
+
+ClientSocket.close()
---
+++ Programs/P32_Mutithreading_Server.py
@@ -0,0 +1,34 @@
+import socket
+import os
+from _thread import *
+
+ServerSocket = socket.socket()
+host = '127.0.0.1'
+port = 1233
+ThreadCount = 0
+try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Waitiing for a Connection..')
+ServerSocket.listen(5)
+
+#Function for handling requests by a thread
+def threaded_client(connection):
+    connection.send(str.encode('Welcome to the Server\n'))
+    while True:
+        data = connection.recv(2048)
+        reply = 'Server Says: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(reply))
+    connection.close()
+
+while True:
+    Client, address = ServerSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSocket.close()
