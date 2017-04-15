f91402f93eb8a27ab417dded418fe1d479614eaf
Omkar Pathak <omkarpathak27@gmail.com>
2017-04-15 15:58:43 +0530

    Added Insert Last functionality

--- Programs/P33_DoublyLinkedList.py
+++ Programs/P33_DoublyLinkedList.py
@@ -34,13 +34,39 @@ class LinkedList(object):
         ''' This function checks whether the list is empty'''
         return self.head == None
 
-    def insert(self, data):
+    def insertFirst(self, data):
         ''' This function inserts a new node in the Linked List '''
         newNode = Node(data)
         if self.head:
             self.head.setPrevious(newNode)
         newNode.setNext(self.head)
         self.head = newNode
+
+    def insertLast(self, data):
+        newNode = Node(data)
+        current = self.head
+        while current.getNext() != None:
+            current = current.getNext()
+        current.setNext(newNode)
+        newNode.setPrevious(current)
+
+    # def insertBetween(self, newItem, item):
+    #     current = self.head
+    #     newNode = Node(newItem)
+    #     previous = None
+    #     found = False
+    #     while not found:
+    #         if current.getData() == item:
+    #             found = True
+    #         else:
+    #             previous = current
+    #             current = current.getNext()
+    #
+    #     if previous == None:
+    #         self.head = current.getPrevious()
+    #     else:
+    #         previous.setNext(newNode)
+    #         newNode.setPrevious(previous)
 
     def getAllData(self):
         ''' This function displays the data elements of the Linked List '''
@@ -70,11 +96,13 @@ class LinkedList(object):
 
 if __name__ == '__main__':
     myList = LinkedList()
-    myList.insert(1)
-    myList.insert(12)
-    myList.insert(32)
-    myList.insert(22)
+    myList.insertFirst(1)
+    myList.insertFirst(12)
+    myList.insertFirst(32)
+    myList.insertFirst(22)
+    myList.insertLast(2)
     myList.remove(12)
-    for i in range(0, 10):
-        myList.insert(i)
+    # myList.insertBetween(12, 22)
+    # for i in range(0, 10):
+    #     myList.insertFirst(i)
     print(myList.getAllData())
