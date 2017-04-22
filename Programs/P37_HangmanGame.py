63db2ebddd0bb022b4fa3522f35963f1521314b0
Omkar Pathak <omkarpathak27@gmail.com>
2017-04-22 18:58:56 +0530

    Python Hangman Game

--- Programs/P37_HangmanGame.py
+++ Programs/P37_HangmanGame.py
@@ -2,6 +2,7 @@
 # This is just an example of how we can use Python for some gaming problems.
 
 import random
+from collections import Counter
 
 someWords = '''apple banana mango strawberry orange grape pineapple apricot lemon coconut watermelon
 cherry papaya berry peach lychee muskmelon'''
@@ -16,13 +17,32 @@ if __name__ == '__main__':
     print()
 
     playing = True
-
     letterGuessed = ''
+    chances = len(word) + 2
+    correct = 0
 
     try:
-        while playing == True:
+        while (chances != 0):
             print()
-            guess = str(input('Enter a letter to guess: '))
+            chances -= 1
+
+            try:
+                guess = str(input('Enter a letter to guess: '))
+            except:
+                print('Enter only a letter!')
+                continue
+
+            # Validation of the guess
+            if not guess.isalpha():
+                print('Enter only a LETTER')
+                continue
+            elif len(guess) > 1:
+                print('Enter only a SINGLE letter')
+                continue
+            elif guess in letterGuessed:
+                print('You have already guessed that letter')
+                continue
+
 
             # If letter is guessed correcly
             if guess in word:
@@ -32,8 +52,21 @@ if __name__ == '__main__':
             for char in word:
                 if char in letterGuessed:
                     print(char, end = ' ')
+                    correct += 1
                 else:
                     print('_', end = ' ')
+
+            # If user has guessed all the letters
+            if (Counter(letterGuessed) == Counter(word)):
+                print()
+                print('Congratulations, You won!')
+                break
+
+        # If user has used all of his chances
+        if chances == 0:
+            print()
+            print('You lost! Try again..')
+            print('The word was {}'.format(word))
 
     except KeyboardInterrupt:
         print()
