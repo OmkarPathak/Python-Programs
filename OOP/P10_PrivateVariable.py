3106dd0b9cd15831aee09dd8e1b903119e28f9d6
Omkar Pathak <omkarpathak27@gmail.com>
2017-04-22 17:32:30 +0530

    Private Variables

--- OOP/P10_PrivateVariable.py
+++ OOP/P10_PrivateVariable.py
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     myObj = Person('Omkar')
     myObj.displayInfo()
     print(myObj.name)                           # Can be accessed as it is public variable
-    # print(myObj.__education)                    # Throws an error
+    # print(myObj.__education)                  # Throws an error
     print(myObj._Person__education)             # Private variable can be accessed like this but NEVER EVER
                                                 # do this please!!!
