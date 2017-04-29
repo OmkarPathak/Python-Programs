7e697e26b96bff0f47224dc55eb8807e86027e83
Omkar Pathak <omkarpathak27@gmail.com>
2017-04-29 09:03:09 +0530

    Synchronization using Pipes

--- Programs/P42_MultiprocessingPipes.py
+++ Programs/P42_MultiprocessingPipes.py
@@ -4,20 +4,22 @@
 from multiprocessing import Process, Pipe
 
 def parentData(parent):
+    ''' This function sends the data for the child process '''
     parent.send(['Hello'])
     parent.close()
 
 def childData(child):
+    ''' This function sends the data for the parent process '''
     child.send(['Bye'])
     child.close()
 
 if __name__ == '__main__':
-    parent, child = Pipe()
-    process1 = Process(target = parentData, args = (parent, ))
-    process2 = Process(target = childData, args = (child, ))
-    process1.start()
-    process2.start()
-    print(parent.recv())
-    print(child.recv())
-    process1.join()
+    parent, child = Pipe()              # Create Pipe
+    process1 = Process(target = parentData, args = (parent, ))      # Create a process for handling parent data
+    process2 = Process(target = childData, args = (child, ))        # Create a process for handling child data
+    process1.start()                    # Start the  parent process
+    process2.start()                    # Start the child process
+    print(parent.recv())                # Display data received from child (BYE)
+    print(child.recv())                 # Display data received from parent (HELLO)
+    process1.join()                     # Wait till the process completes its execution
     process2.join()
