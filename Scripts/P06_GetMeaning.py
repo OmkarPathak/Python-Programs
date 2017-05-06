3ba8a1197cda47f1f66dcdb9dbcaee78f1eb7717
Omkar Pathak <omkarpathak27@gmail.com>
2017-05-06 06:13:34 +0530

    Script for getting dictionary meaning

--- Scripts/P06_GetMeaning.py
+++ Scripts/P06_GetMeaning.py
@@ -6,26 +6,31 @@ from bs4 import BeautifulSoup
 
 word = input('Enter the word of which you want to find the meaning: ')
 
-url = "https://www.vocabulary.com/dictionary/"+word+""
+# Get the meaning by scrapping www.vocabulary.com
+url = "https://www.vocabulary.com/dictionary/" + word + ""
 htmlfile = urllib.request.urlopen(url)
 soup = BeautifulSoup(htmlfile, 'lxml')
 soup1 = soup.find(class_="short")
 
+# If certain word's meaning is not found
 try:
     soup1 = soup1.get_text()
 except AttributeError:
     print('Cannot find such word! Check spelling.')
     exit()
 
-print ('-' * 30 + '->',word,"<-" + "-" * 30)
+# Print short meaning
+print ('-' * 25 + '->',word,"<-" + "-" * 25)
 print ("SHORT MEANING: ",soup1)
 print ('-' * 65)
 
+# Print long meaning
 soup2 = soup.find(class_="long")
 soup2 = soup2.get_text()
 
 print ('-' * 65)
 
+# Print instances like Synonyms, Antonyms, etc.
 soup3 = soup.find(class_="instances")
 txt = soup3.get_text()
 txt1 = txt.rstrip()
