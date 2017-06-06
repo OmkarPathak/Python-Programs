7a0438fc014487c324140ff801d60be46a5d5297
Omkar Pathak <omkarpathak27@gmail.com>
2017-06-06 07:29:29 +0530

    SPOJ Problems

---
+++ CompetitiveProgramming/SPOJ/P07_BSCXOR.py
@@ -0,0 +1,15 @@
+# Given two logic values p and q (0 or 1) please compute p XOR q.
+#
+# Example
+#
+# Input:
+# 1 1
+#
+# Output:
+# 0
+
+a, b = map(int, input().split())
+if a != b:
+    print(1)
+else:
+    print(0)
