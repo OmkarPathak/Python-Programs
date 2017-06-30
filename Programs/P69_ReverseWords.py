d05434d418c94973f7dccc33ddd18f422f8f94ff
Omkar Pathak <omkarpathak27@gmail.com>
2017-06-30 06:56:06 +0530

    Reverse the Words

---
+++ Programs/P69_ReverseWords.py
@@ -0,0 +1,12 @@
+# Author: OMKAR PATHAK
+
+# Python program to reverse the words
+
+userInput = input()
+userInput = userInput.split()
+
+print(' '.join(userInput[::-1]))
+
+# OUTPUT:
+# Computer Science
+# Science Computer
