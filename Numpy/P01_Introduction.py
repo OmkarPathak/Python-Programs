ff2651e94a8a58c60351ab290e1d052cb8913d2c
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-04 07:27:06 +0530

    Intro and Basics of Numpy

--- Numpy/P01_Introduction.py
+++ Numpy/P01_Introduction.py
@@ -57,3 +57,27 @@ print(myArray.size)         # 20
 
 # to check the data we have dtype.
 print(myArray.dtype)        # int64
+
+# zeros creates an array will all zeros
+myArray = np.zeros((3, 4))
+print(myArray)
+
+# [[ 0.  0.  0.  0.]
+#  [ 0.  0.  0.  0.]
+#  [ 0.  0.  0.  0.]]
+
+# ones creates an array with all ones
+myArray = np.ones((3, 4))
+print(myArray)
+
+# [[ 1.  1.  1.  1.]
+#  [ 1.  1.  1.  1.]
+#  [ 1.  1.  1.  1.]]
+
+# numpy random module helps to initialize array with random values
+myArray = np.random.rand(3, 4)
+print(myArray)
+
+# [[ 0.54808903  0.08750717  0.23886267  0.93589283]
+#  [ 0.90750146  0.31197039  0.54013725  0.91092763]
+#  [ 0.38827674  0.04647878  0.15997665  0.94909537]]
