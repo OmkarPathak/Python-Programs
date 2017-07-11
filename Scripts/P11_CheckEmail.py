dcf3e22f48a26bc146b62f21161904c3068dc85b
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-11 07:19:37 +0530

    Development: Script to login to gmail using Python

--- Scripts/P11_CheckEmail.py
+++ Scripts/P11_CheckEmail.py
@@ -6,11 +6,11 @@
 import email, getpass, imaplib, os
 detach_dir = '.'                # directory where to save attachments (default: current)
 # user = input("Enter your Gmail Username:")
-# pwd = getpass.getpass("Enter your Gmail Password: ")
+pwd = getpass.getpass("Enter your Gmail Password: ")
 # connecting to the gmail imap server
 
 mail = imaplib.IMAP4_SSL("imap.gmail.com")
-mail.login('omkarpathak.comp@mmcoe.edu.in', '8149omkar@')
+mail.login('omkarpathak.comp@mmcoe.edu.in', pwd)
 mail.list()
 mail.select('INBOX') # Specify from where to retrieve the email
 
