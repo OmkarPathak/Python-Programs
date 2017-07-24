7caad0a90fca3df049b240a4680aeca09bddb19f
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-24 07:16:34 +0530

    CodeChef: Easy Problems

---
+++ CompetitiveProgramming/CodeChef/P36_SUBGCD.py
@@ -0,0 +1,70 @@
+# Given an array A1,A2...AN, you have to print the size of the largest contiguous subarray such that
+# GCD of all integers in that subarray is 1.
+#
+# Formally,
+# For a subarray Ai,Ai+1...Aj where 1 ≤ i < j ≤ N to be valid: GCD(Ai,Ai+1...Aj) should be 1. You have to
+# print the size of the largest valid subarray.
+#
+#
+# If no valid subarray exists, output -1.
+#
+# Note:A single element is not considered as a subarray according to the definition of this problem.
+#
+# Input
+# First line contains T, the number of testcases. Each testcase consists of N in one line followed by N
+# integers in the next line.
+#
+# Output
+# For each testcase, print the required answer in one line.
+#
+# Constraints
+# 1 ≤ T ≤ 10
+# 2 ≤ N ≤ 105
+# 1 ≤ Ai ≤ 105
+#
+# Example
+# Input:
+# 2
+# 2
+# 7 2
+# 3
+# 2 2 4
+#
+# Output:
+# 2
+# -1
+# Explanation
+# Example case 1.GCD(2,7)=1. So the subarray [A1,A2] is valid.
+#
+# Example case 2.No subarray satisfies.
+#
+
+def gcd(firstNumber, secondNumber):
+    firstNumber = int(firstNumber)
+    secondNumber = int(secondNumber)
+    if firstNumber < secondNumber:
+        smaller = firstNumber
+    else:
+        smaller = secondNumber
+
+    for i in range(1, smaller + 1):
+        if (firstNumber % i == 0) and (secondNumber % i == 0):
+          gcd = i
+
+    return gcd
+
+for _ in range(int(input())):
+    count =  int(input())
+    array = input().split()
+    currentGCD = array[0]
+    flag = 0
+    size = 0
+    for i in array:
+    currentGCD = gcd(i, currentGCD)
+    if currentGCD == 1:
+        flag = 1
+        print(count)
+        break
+
+    if flag == 0:
+    print(-1)
