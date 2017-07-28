a9755bd0b2bdb41573f509df1fa3f8db2cfd5e28
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-28 07:12:59 +0530

    Bug fixes

--- Programs/P07_PrimeNumber.py
+++ Programs/P07_PrimeNumber.py
@@ -3,18 +3,20 @@
 
 def checkPrime(number):
     '''This function checks for prime number'''
+    isPrime = False
     if number == 2:
         print(number, 'is a Prime Number')
     if number > 1:
         for i in range(2, number):
             if number % i == 0:
                 print(number, 'is not a Prime Number')
+                isPrime = False
                 break
             else:
-                print(number, 'is a Prime Number')
-                break
-    else:
-        print(number, 'is not a Prime Number')
+                isPrime = True
+
+        if isPrime:
+            print(number, 'is a Prime Number')
 
 if __name__ == '__main__':
     userInput = int(input('Enter a number to check: '))
