8d764797422bed31951ae5619334f06418e12ce9
Omkar Pathak <omkarpathak27@gmail.com>
2018-08-23 16:20:12 +0530

    More efficient way to solve Pattern Program

--- Programs/P05_Pattern.py
+++ Programs/P05_Pattern.py
@@ -89,23 +89,24 @@ if __name__ == '__main__':
     pattern5(userInput)
     print()
 
-'''
-following is the another approach to solve pattern problems with reduced time complexity 
+    def pattern6(userInput):
+        '''
+        following is the another approach to solve pattern problems with reduced time complexity 
 
-for 
+        for 
 
-*
-**
-***
-****
-*****
-'''
+        *
+        **
+        ***
+        ****
+        *****
+        '''
 
-num = int(input('Enter number for pattern'))
-pattern = '*'
-string = pattern * num
-x = 0
+        num = int(input('Enter number for pattern'))
+        pattern = '*'
+        string = pattern * num
+        x = 0
 
-for i in string:
-    x = x + 1
-    print(string[0:x])
+        for i in string:
+            x = x + 1
+            print(string[0:x])
