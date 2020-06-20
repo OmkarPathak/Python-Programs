8a6233ffab2481672f15b92a4bad481cef4f4d8d
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-06-20 15:45:21 +0530

    alternate program update
    Press enter to start and stop the watch

--- Programs/P19_SimpleStopWatch.py
+++ Programs/P19_SimpleStopWatch.py
@@ -14,3 +14,32 @@ while True:
         endtime = time.time()
         print('Total Time:', round(endtime - starttime, 2),'secs')
         break
+# Press enter to start and stop the watch
+"""
+import time
+
+print('Press Enter to begin, Press Enter again to stop')
+if input()=='':
+    starttime = time.time()
+    print('Started')
+    while True:
+            val=input() #For ENTER
+            if val=='':
+                print('Stopped')
+                endtime = time.time()
+                print('Total Time:', round(endtime - starttime, 2),'secs')
+                break
+                          
+"""
+
+"""
+Output:
+Press Enter to begin, Press Enter again to stop
+
+Started
+
+Stopped
+Total Time: 1.05 secs
+
+"""
+        
