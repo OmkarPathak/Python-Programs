bce21a449f43edc75da010332eb86ed8311fdd96
abhishek5135 <64274954+abhishek5135@users.noreply.github.com>
2020-10-01 10:57:00 +0530

    Update P07_PrimeNumber.py
    using f-string in print function

--- Programs/P07_PrimeNumber.py
+++ Programs/P07_PrimeNumber.py
@@ -1,5 +1,6 @@
 #Author: OMKAR PATHAK
-#This program checks whether the entered number is prime or not
+#This program checks whether the entered number is prime or no
+
 
 def checkPrime(number):
     '''This function checks for prime number'''
@@ -9,14 +10,14 @@ def checkPrime(number):
     if number > 1:
         for i in range(2, number):
             if number % i == 0:
-                print(number, 'is not a Prime Number')
+                print(f"{number} is not a prime Number")
                 isPrime = False
                 break
             else:
                 isPrime = True
 
         if isPrime:
-            print(number, 'is a Prime Number')
+            print(f"{number} is a prime Number")
 
 if __name__ == '__main__':
     userInput = int(input('Enter a number to check: '))
