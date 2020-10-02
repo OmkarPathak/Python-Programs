1e0ac67e0d759875eba96bbde0dcba68f994e234
Tej Pratap Yadav <60545022+tyadav4268@users.noreply.github.com>
2020-10-02 07:52:37 +0530

    Update P72_PythonLambda.py
    Added some lines to demonstrate the use of lambda function in a more compact way.
    for example directly passing the arguments to lambda function.

--- Programs/P72_PythonLambda.py
+++ Programs/P72_PythonLambda.py
@@ -10,14 +10,24 @@
 # expression using these arguments. You can assign the function to a variable to give it a name.
 # The following example of a lambda function returns the sum of its two arguments:
 
-myFunc = lambda x, y: x * y
-# returns 6
-print(myFunc(2, 3))
+myFunc = lambda x, y: x * y  
 
-# example to find squares of all numbers from a list
+print(myFunc(2, 3))                       #output: 6
+
+#Here we are directly creating the function and passing the arguments
+print((lambda x, y: x * y)(2, 3))         #same output i.e 6
+
+print(type(lambda x, y: x * y))           #Output: <class 'function'>
+
+# example to find squares of all numbers of a list
 myList = [i for i in range(10)]
-# returns square of each number
-myFunc = lambda x: x * x
 
-squares = list(map(myFunc, myList))
-print(squares)              # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# returns square of each number
+myFunc2 = lambda x: x * x
+
+squares = list(map(myFunc2, myList))
+print(squares)                              # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+print(list(map(lambda x: x * x, myList)))   #same as above
+
+
