fcd62320056c2227a70870b31bd49c020015510c
2495shweta <72907529+2495shweta@users.noreply.github.com>
2020-10-14 23:27:52 -0700

    Update P01_hello.py

--- Programs/P01_hello.py
+++ Programs/P01_hello.py
@@ -1,9 +1,15 @@
 # Author: OMKAR PATHAK
 # This program prints the entered message
-
+'''
 def justPrint(text):
     '''This function prints the text passed as argument to this function'''
     print(text)
 
 if __name__ == '__main__':
     justPrint('Hello')
+'''
+    
+    
+#method to print the entered message.(without using function)
+text = input("Enter the message that you want to display: ")
+print(text)
