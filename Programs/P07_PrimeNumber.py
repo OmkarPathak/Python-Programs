f71b8c43f1a63e68bfc5a5ce6d8a956330f77363
Rahul Saini <137695929+ProgramR4732@users.noreply.github.com>
2023-09-02 17:09:57 +0530

    Simplified & Optimized P07_PrimeNumber.py

--- Programs/P07_PrimeNumber.py
+++ Programs/P07_PrimeNumber.py
@@ -3,21 +3,14 @@
 
 def checkPrime(number):
     '''This function checks for prime number'''
-    isPrime = False
-    if number == 2:
-        print(number, 'is a Prime Number')
-    if number > 1:
-        for i in range(2, number):
-            if number % i == 0:
-                print(number, 'is not a Prime Number')
-                isPrime = False
-                break
-            else:
-                isPrime = True
 
-        if isPrime:
-            print(number, 'is a Prime Number')
+    for i in range(2, (number // 2) + 1):
+        if not number % i:
+            print(f"{number} is not a Prime Number.")
+            return
+
+    print(f"{number} is a Prime Number")
 
 if __name__ == '__main__':
-    userInput = int(input('Enter a number to check: '))
+    userInput: int = int(input('Enter a number to check: '))
     checkPrime(userInput)
